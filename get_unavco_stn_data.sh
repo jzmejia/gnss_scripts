b290d196767d9d9d779cf7ed6c35f8036c5cd3bd
Jessica Mejia <jzimmerman990@gmail.com>
2025-11-21 15:08:44 -0500

    Update get_unavco_stn_data.sh
    update curl command for current syntax used by EarthScope-cli

--- get_unavco_stn_data.sh
+++ get_unavco_stn_data.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+
+# Requirements
+# ------------
+# earthscope-cli
+# before running script loginto earthscope-cli run in terminal:
+# es login 
+
 # year (YY) and day of year range to download data for
 gps_yr=23
 sta_doy=259
 end_doy=365
+# obs_freq=10 # in days
 # 4 char station name
 stn=hel2
 file_ext="o.Z"
 access_url="https://data.unavco.org/archive/gnss/rinex/obs/20"${gps_yr}
 
-
+# use commented out code below if you want an interval between dates
+# for i in `seq ${sta_doy} ${obs_freq} ${end_doy}`
 for i in `seq ${sta_doy} ${end_doy}`
 do 
   if [ $i -lt 100 ]
@@ -24,5 +33,5 @@ do
 
   file_name=${stn}${gdoy}0.${gps_yr}${file_ext}
   path=${access_url}/${gdoy}/${file_name}
-  curl -L -O -f --url ${path} --header "authorization: Bearer $(es sso access --token)"
+  curl -L -O -f --url ${path} --header "authorization: Bearer $(es user get-access-token)"
 done
